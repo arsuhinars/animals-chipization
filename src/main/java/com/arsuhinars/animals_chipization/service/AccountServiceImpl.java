37f3886eea2ecd953407d185e3e6c6720ca0591a
arsuhinars <arsuhinars@gmail.com>
2023-03-06 17:03:12 +0400

    ✨ Implemented AccountService interface

---
+++ src/main/java/com/arsuhinars/animals_chipization/service/AccountServiceImpl.java
@@ -0,0 +1,88 @@
+package com.arsuhinars.animals_chipization.service;
+
+import com.arsuhinars.animals_chipization.exception.AlreadyExistException;
+import com.arsuhinars.animals_chipization.exception.BoundException;
+import com.arsuhinars.animals_chipization.exception.NotFoundException;
+import com.arsuhinars.animals_chipization.model.Account;
+import com.arsuhinars.animals_chipization.repository.AccountRepository;
+import com.arsuhinars.animals_chipization.schema.account.AccountCreateSchema;
+import com.arsuhinars.animals_chipization.schema.account.AccountSchema;
+import com.arsuhinars.animals_chipization.schema.account.AccountUpdateSchema;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@Service
+public class AccountServiceImpl implements AccountService {
+    @Autowired
+    private AccountRepository repository;
+
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
+    @Override
+    public AccountSchema create(AccountCreateSchema account) throws AlreadyExistException {
+        if (repository.existsByEmail(account.getEmail())) {
+            throw new AlreadyExistException("Account with email \"" + account.getEmail() + "\" already exists");
+        }
+
+        return AccountSchema.createFromModel(
+            repository.save(new Account(
+                account.getFirstName(),
+                account.getLastName(),
+                account.getEmail(),
+                passwordEncoder.encode(account.getPassword()),
+                true
+            ))
+        );
+    }
+
+    @Override
+    public AccountSchema getById(Long id) {
+        return repository.findById(id).map(AccountSchema::createFromModel).orElse(null);
+    }
+
+    @Override
+    public AccountSchema getByEmail(String email) {
+        return repository.findByEmail(email).map(AccountSchema::createFromModel).orElse(null);
+    }
+
+    @Override
+    public AccountSchema update(Long id, AccountUpdateSchema account) throws NotFoundException, AlreadyExistException {
+        var result = repository.findById(id);
+        if (result.isEmpty()) {
+            throw new NotFoundException("Account with id " + id + " was not found");
+        }
+
+        if (
+            !account.getEmail().equals(result.get().getEmail()) &&
+            repository.existsByEmail(account.getEmail())
+        ) {
+            throw new AlreadyExistException("Account with email \"" + account.getEmail() + "\" already exists");
+        }
+
+        var dbAccount = result.get();
+        dbAccount.setFirstName(account.getFirstName());
+        dbAccount.setLastName(account.getLastName());
+        dbAccount.setEmail(account.getEmail());
+        dbAccount.setHashedPassword(
+            passwordEncoder.encode(account.getPassword())
+        );
+
+        return AccountSchema.createFromModel(repository.save(dbAccount));
+    }
+
+    @Override
+    public void delete(Long id) throws NotFoundException, BoundException {
+        var result = repository.findById(id);
+        if (result.isEmpty()) {
+            throw new NotFoundException("Account with id " + id + " was not found");
+        }
+
+        if (!result.get().getChippedAnimals().isEmpty()) {
+            throw new BoundException("Account is chipper for some animals");
+        }
+
+        repository.deleteById(id);
+    }
+}
