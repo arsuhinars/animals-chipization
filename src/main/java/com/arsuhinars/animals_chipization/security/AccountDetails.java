9d0a8055eeb75ee89d7420833140f91967953196
arsuhinars <arsuhinars@gmail.com>
2023-03-06 17:01:28 +0400

    ✨ Added AccountDetails class

---
+++ src/main/java/com/arsuhinars/animals_chipization/security/AccountDetails.java
@@ -0,0 +1,55 @@
+package com.arsuhinars.animals_chipization.security;
+
+import com.arsuhinars.animals_chipization.model.Account;
+import lombok.AccessLevel;
+import lombok.Data;
+import lombok.NonNull;
+import lombok.Setter;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.Collections;
+
+@Data
+public class AccountDetails implements UserDetails {
+    @NonNull
+    @Setter(AccessLevel.PRIVATE)
+    private Account account;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return Collections.singleton(new SimpleGrantedAuthority("USER"));
+    }
+
+    @Override
+    public String getPassword() {
+        return account.getHashedPassword();
+    }
+
+    @Override
+    public String getUsername() {
+        return account.getEmail();
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return false;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return account.getIsActive();
+    }
+}
