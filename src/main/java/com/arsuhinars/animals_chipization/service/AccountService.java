87aeded15e972168e4dae639931820fa73eb124c
arsuhinars <arsuhinars@gmail.com>
2023-03-08 16:04:04 +0400

    :sparkles: Added search method into AccountService

--- src/main/java/com/arsuhinars/animals_chipization/service/AccountService.java
+++ src/main/java/com/arsuhinars/animals_chipization/service/AccountService.java
@@ -7,12 +7,16 @@ import com.arsuhinars.animals_chipization.schema.account.AccountCreateSchema;
 import com.arsuhinars.animals_chipization.schema.account.AccountSchema;
 import com.arsuhinars.animals_chipization.schema.account.AccountUpdateSchema;
 
+import java.util.List;
+
 public interface AccountService {
     AccountSchema create(AccountCreateSchema account) throws AlreadyExistException;
 
     AccountSchema getById(Long id);
 
     AccountSchema getByEmail(String email);
+
+    List<AccountSchema> search(String firstName, String lastName, String email, int from, int count);
 
     AccountSchema update(Long id, AccountUpdateSchema account) throws NotFoundException, AlreadyExistException;
 
