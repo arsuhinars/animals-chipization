e98316cf833f676a2ff3361ca23cbdae2d3b9965
arsuhinars <arsuhinars@gmail.com>
2023-03-08 16:05:00 +0400

    :sparkles: Implemented search method in AccountServiceImpl

--- src/main/java/com/arsuhinars/animals_chipization/service/AccountServiceImpl.java
+++ src/main/java/com/arsuhinars/animals_chipization/service/AccountServiceImpl.java
@@ -8,9 +8,12 @@ import com.arsuhinars.animals_chipization.repository.AccountRepository;
 import com.arsuhinars.animals_chipization.schema.account.AccountCreateSchema;
 import com.arsuhinars.animals_chipization.schema.account.AccountSchema;
 import com.arsuhinars.animals_chipization.schema.account.AccountUpdateSchema;
+import com.arsuhinars.animals_chipization.util.OffsetPageable;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class AccountServiceImpl implements AccountService {
@@ -45,6 +48,14 @@ public class AccountServiceImpl implements AccountService {
     @Override
     public AccountSchema getByEmail(String email) {
         return repository.findByEmail(email).map(AccountSchema::createFromModel).orElse(null);
+    }
+
+    @Override
+    public List<AccountSchema> search(String firstName, String lastName, String email, int from, int count) {
+        return repository.search(firstName, lastName, email, new OffsetPageable(count, from))
+            .stream()
+            .map(AccountSchema::createFromModel)
+            .toList();
     }
 
     @Override
