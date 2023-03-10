95b7c1d1674b52dfcd1175adc1043c9b81c5ae77
arsuhinars <arsuhinars@gmail.com>
2023-03-10 10:49:28 +0400

    :sparkles: Added methods in AnimalService

--- src/main/java/com/arsuhinars/animals_chipization/service/AnimalService.java
+++ src/main/java/com/arsuhinars/animals_chipization/service/AnimalService.java
@@ -1,14 +1,29 @@
 package com.arsuhinars.animals_chipization.service;
 
 import com.arsuhinars.animals_chipization.exception.*;
+import com.arsuhinars.animals_chipization.model.AnimalGender;
+import com.arsuhinars.animals_chipization.model.LifeStatus;
 import com.arsuhinars.animals_chipization.schema.animal.AnimalCreateSchema;
 import com.arsuhinars.animals_chipization.schema.animal.AnimalSchema;
 import com.arsuhinars.animals_chipization.schema.animal.AnimalUpdateSchema;
 
+import java.time.LocalDateTime;
+import java.util.List;
+
 public interface AnimalService {
-    AnimalSchema create(AnimalCreateSchema animal) throws NotFoundException;
+    AnimalSchema create(AnimalCreateSchema animal) throws NotFoundException, AlreadyExistException;
 
     AnimalSchema getById(Long id);
+
+    List<AnimalSchema> search(
+        LocalDateTime start,
+        LocalDateTime end,
+        Long chipperId,
+        Long chippingLocationId,
+        LifeStatus lifeStatus,
+        AnimalGender gender,
+        int from, int size
+    );
 
     AnimalSchema update(Long id, AnimalUpdateSchema animal) throws NotFoundException;
 
