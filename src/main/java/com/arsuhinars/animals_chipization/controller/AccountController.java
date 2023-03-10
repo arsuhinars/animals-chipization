4e261a11fb0d006359612143b58b346c7b276893
arsuhinars <arsuhinars@gmail.com>
2023-03-10 10:51:04 +0400

    :sparkles: Added new methods in AccountController

--- src/main/java/com/arsuhinars/animals_chipization/controller/AccountController.java
+++ src/main/java/com/arsuhinars/animals_chipization/controller/AccountController.java
@@ -1,25 +1,31 @@
 package com.arsuhinars.animals_chipization.controller;
 
+import com.arsuhinars.animals_chipization.exception.AlreadyExistException;
+import com.arsuhinars.animals_chipization.exception.ForbiddenException;
 import com.arsuhinars.animals_chipization.exception.NotFoundException;
 import com.arsuhinars.animals_chipization.schema.account.AccountSchema;
+import com.arsuhinars.animals_chipization.schema.account.AccountUpdateSchema;
+import com.arsuhinars.animals_chipization.security.AccountDetails;
 import com.arsuhinars.animals_chipization.service.AccountService;
 import jakarta.validation.constraints.Min;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Objects;
 
 @RestController
 @RequestMapping("/accounts")
 @Validated
 public class AccountController {
     @Autowired
-    private AccountService accountService;
+    private AccountService service;
 
     @GetMapping("/{id}")
     public AccountSchema getAccountById(@PathVariable @Min(1) Long id) throws NotFoundException {
-        var account = accountService.getById(id);
+        var account = service.getById(id);
         if (account == null) {
             throw new NotFoundException("Account was not found");
         }
@@ -35,6 +41,36 @@ public class AccountController {
         @RequestParam(defaultValue = "0") @Min(0) Integer from,
         @RequestParam(defaultValue = "10") @Min(1) Integer size
     ) {
-        return accountService.search(firstName, lastName, email, from, size);
+        return service.search(firstName, lastName, email, from, size);
+    }
+
+    @PutMapping("/{id}")
+    public AccountSchema updateAccountById(
+        @PathVariable @Min(1) Long id,
+        AccountUpdateSchema account,
+        Authentication authentication
+    ) throws AlreadyExistException, ForbiddenException {
+        var details = (AccountDetails)authentication.getDetails();
+
+        if (!Objects.equals(details.getAccount().getId(), id)) {
+            throw new ForbiddenException("You can only update your own account");
+        }
+
+        return service.update(id, account);
+    }
+
+    @DeleteMapping
+    public void deleteAccountById(
+        @PathVariable @Min(1) Long id,
+        AccountUpdateSchema account,
+        Authentication authentication
+    ) {
+        var details = (AccountDetails)authentication.getDetails();
+
+        if (!Objects.equals(details.getAccount().getId(), id)) {
+            throw new ForbiddenException("You can only delete your own account");
+        }
+
+        service.delete(id);
     }
 }
