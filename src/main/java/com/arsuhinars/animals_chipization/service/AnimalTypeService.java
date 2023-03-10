9be06c911999559fab8da18bc6f19ca6bf4ecfbf
arsuhinars <arsuhinars@gmail.com>
2023-03-10 10:50:30 +0400

    :art: Improved methods in AnimalTypeService

--- src/main/java/com/arsuhinars/animals_chipization/service/AnimalTypeService.java
+++ src/main/java/com/arsuhinars/animals_chipization/service/AnimalTypeService.java
@@ -3,14 +3,14 @@ package com.arsuhinars.animals_chipization.service;
 import com.arsuhinars.animals_chipization.exception.AlreadyExistException;
 import com.arsuhinars.animals_chipization.exception.BoundException;
 import com.arsuhinars.animals_chipization.exception.NotFoundException;
-import com.arsuhinars.animals_chipization.schema.animal.AnimalTypeSchema;
+import com.arsuhinars.animals_chipization.schema.animal.type.AnimalTypeSchema;
 
 public interface AnimalTypeService {
-    AnimalTypeSchema create(AnimalTypeSchema type) throws AlreadyExistException;
+    AnimalTypeSchema create(AnimalTypeSchema animalType) throws AlreadyExistException;
 
     AnimalTypeSchema getById(Long id);
 
-    AnimalTypeSchema update(AnimalTypeSchema type) throws NotFoundException, AlreadyExistException;
+    AnimalTypeSchema update(Long id, AnimalTypeSchema animalType) throws NotFoundException, AlreadyExistException;
 
     void delete(Long id) throws NotFoundException, BoundException;
 }
