c8d5287d925231cafbc0871e2b12ef83d283d173
arsuhinars <arsuhinars@gmail.com>
2023-03-12 00:15:01 +0400

    :bug: LocationServiceImpl now is annotated with @Service

--- src/main/java/com/arsuhinars/animals_chipization/service/LocationServiceImpl.java
+++ src/main/java/com/arsuhinars/animals_chipization/service/LocationServiceImpl.java
@@ -7,7 +7,9 @@ import com.arsuhinars.animals_chipization.model.Location;
 import com.arsuhinars.animals_chipization.repository.LocationRepository;
 import com.arsuhinars.animals_chipization.schema.LocationSchema;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
+@Service
 public class LocationServiceImpl implements LocationService {
     @Autowired
     private LocationRepository repository;
