3c021e57f868b63dbeb54023141d48f69493be13
arsuhinars <arsuhinars@gmail.com>
2023-03-12 00:15:33 +0400

    :bug: RegisterController fixes

--- src/main/java/com/arsuhinars/animals_chipization/controller/RegisterController.java
+++ src/main/java/com/arsuhinars/animals_chipization/controller/RegisterController.java
@@ -4,20 +4,21 @@ import com.arsuhinars.animals_chipization.exception.AlreadyExistException;
 import com.arsuhinars.animals_chipization.schema.account.AccountCreateSchema;
 import com.arsuhinars.animals_chipization.schema.account.AccountSchema;
 import com.arsuhinars.animals_chipization.service.AccountService;
+import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.validation.annotation.Validated;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping(value = "/registration")
-@Validated
 public class RegisterController {
     @Autowired
     private AccountService accountService;
 
     @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
     public AccountSchema registerAccount(
-        @RequestBody AccountCreateSchema account
+        @Valid @RequestBody AccountCreateSchema account
     ) throws AlreadyExistException {
         return accountService.create(account);
     }
