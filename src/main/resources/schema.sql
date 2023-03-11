7398e0d7a2917b2b77f6fb1131ea2292689cb843
arsuhinars <arsuhinars@gmail.com>
2023-03-12 00:06:16 +0400

    :wrench: schema.sql update

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,5 +1,5 @@
 CREATE EXTENSION IF NOT EXISTS pg_trgm;
 
-CREATE INDEX accs_first_name_idx ON accounts USING GIST (first_name gist_trgm_ops);
-CREATE INDEX accs_last_name_idx ON accounts USING GIST (last_name gist_trgm_ops);
-CREATE INDEX accs_email_idx ON accounts USING GIST (email gist_trgm_ops);
+CREATE INDEX accs_first_name_idx ON accounts USING GIN (LOWER(first_name) gin_trgm_ops);
+CREATE INDEX accs_last_name_idx ON accounts USING GIN (LOWER(last_name) gin_trgm_ops);
+CREATE INDEX accs_email_idx ON accounts USING GIN (LOWER(email) gin_trgm_ops);
