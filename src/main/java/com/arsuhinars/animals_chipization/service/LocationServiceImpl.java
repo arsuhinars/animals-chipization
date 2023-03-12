b94082f543de2f7f8567cafafa44c01035e8ee47
arsuhinars <arsuhinars@gmail.com>
2023-03-12 18:27:51 +0400

    :art: Improved structure of LocationServiceImpl

--- src/main/java/com/arsuhinars/animals_chipization/service/LocationServiceImpl.java
+++ src/main/java/com/arsuhinars/animals_chipization/service/LocationServiceImpl.java
@@ -1,13 +1,17 @@
 package com.arsuhinars.animals_chipization.service;
 
 import com.arsuhinars.animals_chipization.exception.AlreadyExistException;
-import com.arsuhinars.animals_chipization.exception.BoundException;
+import com.arsuhinars.animals_chipization.exception.DependsOnException;
 import com.arsuhinars.animals_chipization.exception.NotFoundException;
+import com.arsuhinars.animals_chipization.model.Animal;
 import com.arsuhinars.animals_chipization.model.Location;
 import com.arsuhinars.animals_chipization.repository.LocationRepository;
 import com.arsuhinars.animals_chipization.schema.LocationSchema;
+import com.arsuhinars.animals_chipization.util.ErrorDetailsFormatter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class LocationServiceImpl implements LocationService {
@@ -17,18 +21,25 @@ public class LocationServiceImpl implements LocationService {
     @Override
     public LocationSchema create(LocationSchema location) throws AlreadyExistException {
         if (repository.existsByLatitudeAndLongitude(
-            location.getLatitude(),
-            location.getLongitude())
-        ) {
-            throw new AlreadyExistException("Location with given latitude and longitude already exists");
-        }
-
-        return LocationSchema.createFromModel(
-            repository.save(new Location(
                 location.getLatitude(),
                 location.getLongitude()
-            ))
+            )
+        ) {
+            throw new AlreadyExistException(
+                ErrorDetailsFormatter.formatAlreadyExistsError(
+                    Location.class,
+                    "[latitude, longitude]",
+                    List.of(location.getLatitude(), location.getLongitude())
+                )
+            );
+        }
+
+        var dbLocation = new Location(
+            location.getLatitude(),
+            location.getLongitude()
         );
+
+        return LocationSchema.createFromModel(repository.save(dbLocation));
     }
 
     @Override
@@ -38,19 +49,27 @@ public class LocationServiceImpl implements LocationService {
 
     @Override
     public LocationSchema update(Long id, LocationSchema location) throws NotFoundException, AlreadyExistException {
-        var result = repository.findById(id);
-        if (result.isEmpty()) {
-            throw new NotFoundException("Location with id " + id + " was not found");
+        var dbLocation = repository.findById(id).orElse(null);
+        if (dbLocation == null) {
+            throw new NotFoundException(
+                ErrorDetailsFormatter.formatNotFoundError(Location.class, id)
+            );
         }
 
         if (repository.existsByLatitudeAndLongitude(
-            location.getLatitude(),
-            location.getLongitude())
+                location.getLatitude(),
+                location.getLongitude()
+            )
         ) {
-            throw new AlreadyExistException("Location with given latitude and longitude already exists");
+            throw new AlreadyExistException(
+                ErrorDetailsFormatter.formatAlreadyExistsError(
+                    Location.class,
+                    "[latitude, longitude]",
+                    List.of(location.getLatitude(), location.getLongitude())
+                )
+            );
         }
 
-        var dbLocation = result.get();
         dbLocation.setLatitude(location.getLatitude());
         dbLocation.setLongitude(location.getLongitude());
 
@@ -58,18 +77,22 @@ public class LocationServiceImpl implements LocationService {
     }
 
     @Override
-    public void delete(Long id) throws NotFoundException, BoundException {
-        var result = repository.findById(id);
-        if (result.isEmpty()) {
-            throw new NotFoundException("Location with id " + id + " was not found");
+    public void delete(Long id) throws NotFoundException, DependsOnException {
+        var dbLocation = repository.findById(id).orElse(null);
+        if (dbLocation == null) {
+            throw new NotFoundException(
+                ErrorDetailsFormatter.formatNotFoundError(Location.class, id)
+            );
         }
 
-        if (!result.get().getChippedAnimals().isEmpty() ||
-            !result.get().getVisitedAnimals().isEmpty()
+        if (!dbLocation.getChippedAnimals().isEmpty() ||
+            !dbLocation.getVisitedAnimals().isEmpty()
         ) {
-            throw new BoundException("Location is bound with some animals");
+            throw new DependsOnException(
+                ErrorDetailsFormatter.formatDependsOnError(dbLocation, Animal.class)
+            );
         }
 
-        repository.deleteById(id);
+        repository.delete(dbLocation);
     }
 }
