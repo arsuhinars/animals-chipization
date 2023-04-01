408730afb8db4e359dd81e7133a3253f0600f454
arsuhinars <arsuhinars@gmail.com>
2023-04-01 21:53:46 +0400

    :art: Improved LocationSchema class

--- src/main/java/com/arsuhinars/animals_chipization/schema/LocationSchema.java
+++ src/main/java/com/arsuhinars/animals_chipization/schema/LocationSchema.java
@@ -1,38 +1,36 @@
 package com.arsuhinars.animals_chipization.schema;
 
 import com.arsuhinars.animals_chipization.model.Location;
+import com.arsuhinars.animals_chipization.validation.FloatingMax;
+import com.arsuhinars.animals_chipization.validation.FloatingMin;
 import jakarta.validation.constraints.Max;
 import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
+@AllArgsConstructor
 public class LocationSchema {
+    @NotNull
+    @Min(1)
     private Long id;
 
     @NotNull
-    @Min(-90)
-    @Max(90)
+    @FloatingMin(value = -90, inclusive = true)
+    @FloatingMax(value = 90, inclusive = true)
     private Double latitude;
 
     @NotNull
-    @Min(-180)
-    @Max(180)
+    @FloatingMin(value = -180, inclusive = true)
+    @FloatingMax(value = 180, inclusive = true)
     private Double longitude;
 
-    public LocationSchema(Long id, Double latitude, Double longitude) {
-        this.id = id;
-        this.latitude = latitude;
-        this.longitude = longitude;
-    }
-
-    public static LocationSchema createFromModel(Location location) {
-        return new LocationSchema(
-            location.getId(),
-            location.getLatitude(),
-            location.getLongitude()
-        );
+    public LocationSchema(Location location) {
+        this.id = location.getId();
+        this.latitude = location.getLatitude();
+        this.longitude = location.getLongitude();
     }
 }
