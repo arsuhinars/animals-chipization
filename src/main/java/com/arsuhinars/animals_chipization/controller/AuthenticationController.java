a97e38733814b043281d8e0fc300893f66eae5ef
arsuhinars <arsuhinars@gmail.com>
2023-04-02 12:36:19 +0400

    :art: Renamed and improved AuthenticationController

--- src/main/java/com/arsuhinars/animals_chipization/controller/AuthenticationController.java
+++ src/main/java/com/arsuhinars/animals_chipization/controller/AuthenticationController.java
@@ -5,21 +5,23 @@ import com.arsuhinars.animals_chipization.schema.account.AccountCreateSchema;
 import com.arsuhinars.animals_chipization.schema.account.AccountSchema;
 import com.arsuhinars.animals_chipization.service.AccountService;
 import jakarta.validation.Valid;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping(value = "/registration")
-public class RegisterController {
-    @Autowired
-    private AccountService accountService;
+public class AuthenticationController {
+    private final AccountService service;
+
+    public AuthenticationController(AccountService service) {
+        this.service = service;
+    }
 
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
     public AccountSchema registerAccount(
         @Valid @RequestBody AccountCreateSchema account
     ) throws AlreadyExistException {
-        return accountService.create(account);
+        return service.create(account);
     }
 }
