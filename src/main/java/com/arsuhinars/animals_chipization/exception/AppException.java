fb4a94c67d064d6c3d214a93309058301042593c
arsuhinars <arsuhinars@gmail.com>
2023-04-02 12:36:32 +0400

    :art: Minor improvements in AppException class

--- src/main/java/com/arsuhinars/animals_chipization/exception/AppException.java
+++ src/main/java/com/arsuhinars/animals_chipization/exception/AppException.java
@@ -3,19 +3,16 @@ package com.arsuhinars.animals_chipization.exception;
 import lombok.*;
 import org.springframework.http.HttpStatus;
 
-@Setter
 @Getter
 public class AppException extends RuntimeException {
-    @Setter(AccessLevel.PRIVATE)
-    private String details;
+    private final String details;
 
-    @Setter(AccessLevel.PRIVATE)
-    private HttpStatus statusCode;
+    private final HttpStatus statusCode;
 
     public AppException(String details, HttpStatus statusCode) {
         super(details);
 
-        setDetails(details);
-        setStatusCode(statusCode);
+        this.details = details;
+        this.statusCode = statusCode;
     }
 }
