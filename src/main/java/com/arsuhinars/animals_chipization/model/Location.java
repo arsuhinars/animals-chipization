b141f8901de94b27f004af380ade608dc864600d
arsuhinars <arsuhinars@gmail.com>
2023-04-04 12:19:55 +0400

    :bug: Fixed Location model

--- src/main/java/com/arsuhinars/animals_chipization/model/Location.java
+++ src/main/java/com/arsuhinars/animals_chipization/model/Location.java
@@ -6,7 +6,7 @@ import lombok.*;
 import java.util.Set;
 
 @Entity
-@Table(name = "animals_types", indexes = {
+@Table(name = "locations", indexes = {
     @Index(columnList = "latitude, latitude", unique = true)
 })
 @NoArgsConstructor
