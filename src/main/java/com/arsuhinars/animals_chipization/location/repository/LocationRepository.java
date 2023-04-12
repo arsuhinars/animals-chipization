c8d77a3eaa80cedd0885a561f3b2f8296fe3f129
arsuhinars <arsuhinars@gmail.com>
2023-04-12 19:41:25 +0400

    :art: Improved and fixed LocationRepository

--- src/main/java/com/arsuhinars/animals_chipization/location/repository/LocationRepository.java
+++ src/main/java/com/arsuhinars/animals_chipization/location/repository/LocationRepository.java
@@ -1,8 +1,23 @@
 package com.arsuhinars.animals_chipization.location.repository;
 
 import com.arsuhinars.animals_chipization.location.model.Location;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.List;
+
 public interface LocationRepository extends CrudRepository<Location, Long> {
+    @Query("""
+        SELECT
+            CASE WHEN COUNT(*) > 0 THEN TRUE ELSE FALSE END
+        FROM Location loc WHERE loc.position.latitude = ?1 AND loc.position.longitude = ?2
+        """)
     boolean existsByLatitudeAndLongitude(Double latitude, Double longitude);
+
+    @Query("""
+        SELECT loc FROM Location loc WHERE
+            loc.position.latitude >= :minX AND loc.position.latitude <= :maxX AND
+            loc.position.longitude >= :minY AND loc.position.longitude <= :maxY
+        """)
+    List<Location> findInRange(double minX, double minY, double maxX, double maxY);
 }
